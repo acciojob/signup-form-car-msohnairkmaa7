e7f93985a8241e66587ea5d0cd35b7b5eec93e8a
msohnairkmaa7 <129722920+msohnairkmaa7@users.noreply.github.com>
2024-03-22 20:42:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 it("Button for submitting the form exists", () => {
   cy.get("form").within(() => {
+    cy.wait(1000); // Adjust the wait time as needed
     cy.get("input[type='submit']").should("exist");
   });
 });
